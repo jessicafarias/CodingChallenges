c0e5e62e80602ea2a0541d889a259b683ce120b7
jessica <jessica.farias.rosado@gmail.com>
2020-08-09 18:42:06 -0500

    Fix rubocop issues

--- EasyChallenges/pickingNumbers.rb
+++ EasyChallenges/pickingNumbers.rb
@@ -1,14 +1,12 @@
-def pickingNumbers(a)
-    comb = 0
-    guardar =0
-    p array = Array.new(a).uniq!
-    array.each do |x|
-        comp1 = a.count(x+1) + a.count(x)
-        comp2 = a.count(x-1) + a.count(x)
-        guardar = comp1 > comp2 ? comp1 : comp2
-        comb = guardar if comb < guardar
-    end
-    comb
+def picking_numbers(arr)
+  comb = 0
+  guardar = 0
+  p array = Array.new(arr).uniq!
+  array.each do |x|
+    comp1 = arr.count(x + 1) + arr.count(x)
+    comp2 = arr.count(x - 1) + arr.count(x)
+    guardar = comp1 > comp2 ? comp1 : comp2
+    comb = guardar if comb < guardar
+  end
+  comb
 end
-
-p pickingNumbers([1,1,2,2,4,4,5,5,5])
