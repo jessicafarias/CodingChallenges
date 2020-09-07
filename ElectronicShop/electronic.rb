df513a6543b27e908b90f9634dd609e88621b7a2
jessica <jessica.farias.rosado@gmail.com>
2020-09-07 13:04:48 -0500

    ElectronicShop Challenge

---
+++ ElectronicShop/electronic.rb
@@ -0,0 +1,15 @@
+def getMoneySpent(keyboards, drives, b)
+    to_buy = -1
+    keyboards.each do |x|
+        drives.each do |y|
+            if x + y <= b && x + y > to_buy
+                to_buy = x + y
+            end
+        end
+    end
+    return to_buy
+end
+
+def getMoneySpent(keyboards, drives, b)
+    keyboards.
+end
