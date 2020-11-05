6d891188e7e0f35c47eb2ef31ecbffa779b068f5
jessica <jessica.farias.rosado@gmail.com>
2020-11-05 09:11:09 -0600

    Adding Median Challenge

--- Chocolate/chocolate.rb
+++ Chocolate/chocolate.rb
@@ -1,11 +1,18 @@
 def chocolateFeast(n, c, m)
-    var = n/c 
-    var += n/c/m # column2
-    res= (((n/c)%m) + n/c/m)/m # column3
+    #n = dollars you have
+    #c = cost of each chocolate
+    #m = number of chocolate wrapper you need to get a new chocolat bar free
+    var = n/c #number of new chocolates i can buy
+    var += n/c/m # column2  nuber of chocolates i can change for wrappers
+    res= (((n/c)%m) + n/c/m)/m # column3 NEW CHOCOLATES
     var += res
     res2 =(((n/c)%m) + n/c/m)%m
     var += (res + res2 )/m #column4
     var +=(((res + res2 )%m) + ((res + res2 )/m))/m
+end
+
+def recursion(A, m)
+    (A%m + A/m)/m == 0
 end
 p chocolateFeast(10, 2, 5)#6
 p chocolateFeast(6, 2, 2)#5
---
+++ Median/median.rb
@@ -0,0 +1,5 @@
+def findMedian(arr)
+    arr.sort[(arr.length/2)]
+end
+
+p findMedian([1,2,3,4,5,6,7])
